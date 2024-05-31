13341e0cd89be065a9c5aab84f5c05fb8eac94ee
Joao Gomes <contato@joaofelipegomes.com.br>
2024-05-31 17:34:41 -0300

    Init

---
+++ site/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-white": "#FEFFFB",
+        "primary-black": "#010004",
+        "primary-pink": "#2665F6",
+        "secondary-green": "#01A451"
+      }
+    },
+  },
+  plugins: [],
+}
